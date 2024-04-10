9bfcbe311338ca6635ca31bb7bd99fe126b2182c
gbw30 <166647742+gbw30@users.noreply.github.com>
2024-04-10 18:30:35 -0400

    Decoder by Gabriel Wong

--- Lab_9_GitHub_decoder.py
+++ Lab_9_GitHub_decoder.py
@@ -5,6 +5,6 @@ def decode(password: str) -> str:
     for i in password:
         decoded = int(i) - 3
         if decoded < 0:
-            decoded = decoded * -1
+            decoded = decoded + 10
         decoded_password += str(decoded)
     return decoded_password
