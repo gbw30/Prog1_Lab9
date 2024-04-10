64d7d9dd2c120dff9bf4e37b91792fbbd98d0f32
gbw30 <166647742+gbw30@users.noreply.github.com>
2024-04-10 17:03:50 -0400

    Decoder done yay

---
+++ Lab_9_GitHub_decoder.py
@@ -0,0 +1,10 @@
+# Decoder Lab 9 COP3502C
+
+def decode(password: str) -> str:
+    decoded_password = ' '
+    for i in password:
+        decoded = int(i) - 3
+        if decoded < 0:
+            decoded = decoded * -1
+        decoded_password += str(decoded)
+    return decoded_password
