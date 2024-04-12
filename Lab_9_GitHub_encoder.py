b40af5a9d17562fbf652545ab880183ad029ce22
Jayyuan0921 <jay09yuan@gmail.com>
2024-04-12 15:33:18 -0400

    updated decode function

--- Lab_9_GitHub_encoder.py
+++ Lab_9_GitHub_encoder.py
@@ -8,3 +8,11 @@ def encode(password: str) -> str:
             encoded = str(encoded)[1:]
         encoded_password += str(encoded)
     return encoded_password
+
+
+def decode_password(encoded_password):
+    decoded_password = ""
+    for digit in encoded_password:
+        decoded_digit = str((int(digit) - 3) % 10)
+        decoded_password += decoded_digit
+    return decoded_password
